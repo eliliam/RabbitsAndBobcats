a887a2d8432a808a85e034dd3e500cece162b379
Eli Smith <plunkinguitar@gmail.com>
2018-04-14 21:45:25 -1000

    Added animation support

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <h1>Rabbits and Bobcats<h1>
 
 This is a simple project used to calculate and graph a population of rabbits and bobcats over a set period of generations.
-<br><br>To use this project, simply clone the repo and configure your changes in the top of `main.js`.
+<br><br>To use this project, simply clone the repo, run `npm install` and configure your changes in the top of `main.js`.
 <br>Run `npm start` to start the application. It will print its results to console.
 <br><br>An example output
 <br>
--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 let colors = require('colors');
 let asciichart = require('asciichart');
+let sleep = require('sleep');
 
 const rabbitSize = 1; // Physical space taken up by rabbits
 const bobcatSize = 3; // Physical space taken up by bobcats, this means bobcats are 4x4
 const mapSize = 24; // Map size in units
-const gens = 40; // Amount of generations to run through
+const gens = 100; // Amount of generations to run through
+const granularity = 10; // Number of steps to graph
+const showAnimation = true; // Show generations as they happen, set to false to only see last generation
+const animationSpeed = 100; // Millisecond delay between rewrites for animation if applicable
 
-const maxRabbits = 300; // Carrying capacity of rabbits
+const maxRabbits = 0 || (mapSize**2)/2; // Carrying capacity of rabbits
 
 let initRabbitCount = 3; // Amount of starting rabbits, also used as a counter later on
 let bobcats = 1; // Amount of starting bobcats, also used as a counter later on
@@ -28,6 +32,11 @@ for (let r = 0; r < initRabbitCount; r++) {
     }
 }
 
+// Function to clear the console
+console.reset = function () {
+    return process.stdout.write('\033c');
+};
+
 // Function to return amount of rabbits alive at a given time on a map
 function getAlive(curMap){
     let aliveCount = curMap.map((row, indexX) => {
@@ -45,7 +54,7 @@ function getAlive(curMap){
 // Takes map and doubles the population of rabbits, distributed randomly
 function spawnNewRabbits(curMap){
     let startingPop = getAlive(curMap) || 3;
-    for (let r = 0; r < startingPop && startingPop <= 150; r++) {
+    for (let r = 0; r < startingPop && startingPop <= maxRabbits/2; r++) {
         let x = Math.ceil(Math.random() * mapSize - rabbitSize);
         let y = Math.ceil(Math.random() * mapSize - rabbitSize);
         if (!curMap[x][y]) {
@@ -86,7 +95,7 @@ let bobcatsOverTime = [];
 for (let gen = 0; gen < gens; gen++) {
     let res = doGeneration(map, bobcats);
     let newBobcats = Math.floor(res[1] / 3);
-    if (newBobcats < bobcats){
+    if (newBobcats < bobcats || bobcats === 0){
         bobcats = 1;
     } else {
         bobcats += newBobcats - bobcats;
@@ -94,6 +103,19 @@ for (let gen = 0; gen < gens; gen++) {
     map = res[0];
     eatenOverTime.push(res[1]);
     bobcatsOverTime.push(bobcats);
+    console.clear();
+    for (let x = 0; x < map.length; x++){
+        let printStr = "";
+        for (let y = 0; y < map[x].length; y++){
+            if (map[x][y]){
+                printStr += 'r'.toString().green+" ";
+            } else {
+                printStr += 'e'.toString().red+" ";
+            }
+        }
+        console.log(printStr);
+    }
+    if (showAnimation) sleep.msleep(animationSpeed);
 }
 
 // Calculates max values of arrays for pretty plotting
@@ -101,25 +123,34 @@ const maxEaten = Math.max.apply(null, eatenOverTime);
 const maxBobcats = Math.max.apply(null, bobcatsOverTime);
 
 // Processes values into a graphable format
-const rfinal = eatenOverTime.map((val) => (val / maxEaten)*10);
-const bfinal = bobcatsOverTime.map((val) => ((val-1) / (maxBobcats-1))*10);
+const rfinal = eatenOverTime.map((val) => (val / maxEaten)*granularity);
+const bfinal = (maxBobcats === 1) ? bobcatsOverTime : bobcatsOverTime.map((val) => ((val-1) / (maxBobcats-1))*granularity);
 
 // Prints the ending game map, 'r' means rabbit, 'e' means empty
-console.log(("Final Map after " + gens + " iterations").blue);
-for (let x = 0; x < map.length; x++){
-    let printStr = "";
-    for (let y = 0; y < map[x].length; y++){
-        if (map[x][y]){
-            printStr += 'r'.toString().green+" ";
-        } else {
-            printStr += 'e'.toString().red+" ";
-        }
-    }
-    console.log(printStr);
-}
+// console.log(("Final Map after " + gens + " iterations").blue);
+// for (let x = 0; x < map.length; x++){
+//     let printStr = "";
+//     for (let y = 0; y < map[x].length; y++){
+//         if (map[x][y]){
+//             printStr += 'r'.toString().green+" ";
+//         } else {
+//             printStr += 'e'.toString().red+" ";
+//         }
+//     }
+//     console.log(printStr);
+// }
 
 // Prints ascii graphs of Rabbits eaten and bobcats alive over the generations
-console.log(("Rabbits eaten over "+gens+" generations").blue);
+console.log(("Percent Rabbits eaten over "+gens+" generations").blue);
 console.log(asciichart.plot(rfinal));
-console.log(("Bobcats alive over time "+gens+" generations").blue);
-console.log(asciichart.plot(bfinal));
+console.log(("Percent Bobcats alive over time "+gens+" generations").blue);
+// ASCII Chart breaks with an array with only 1s, so we spoof a chart showing only 1s if that is the case
+if (maxBobcats === 1){
+    let bstring = "       1.00 ┼─";
+    for (let i = 0; i < gens-2; i++){
+        bstring += '─';
+    }
+    console.log(bstring);
+} else {
+    console.log(asciichart.plot(bfinal));
+}
